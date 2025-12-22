c630d9405f149dce76384e28de6fe826f796739a
Alex Childers <42629593+abchilders@users.noreply.github.com>
2025-12-22 23:39:58 +1300

    Outline notes for selecting/transforming data

---
+++ DataCamp - Data Manipulation with dplyr/3 - Selecting and Transforming Data.R
@@ -0,0 +1,22 @@
+# Section 3 - Selecting and Transforming Data
+
+# ---SUMMARY---
+# (put which ones to use when here)
+
+# [insert name for helper functions]
+
+# ---SELECT---
+# * select(col1, col2, col3:col6, ...): returns the selected columns
+#    * col3:col6 : select all columns between col3 and col6
+#    * -col_name: 
+# * Select helpers: 
+#    * contains("word")
+#    * starts_with("word")
+#    * ends_with("word")
+#    * last_col(): selects last column in the table
+#    * matches()
+# * ?select_helpers: returns more info
+
+# ---RENAME---
+
+# ---RELOCATE---
